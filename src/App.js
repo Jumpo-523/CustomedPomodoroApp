1eb3dc9f6eb5d7f95449c370be14d12804198535
j.takubo <nyk523jt@gmail.com>
2020-11-05 13:13:48 +0900

    グルグルの絵を導入

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import logo from './logo.svg';
+import senko from './senko.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={senko} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
