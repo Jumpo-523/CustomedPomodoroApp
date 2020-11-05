cfc7756631515696fc21cc31a22dfa41501686e4
j.takubo <nyk523jt@gmail.com>
2020-11-06 00:50:04 +0900

    learn the lifecycle event.

--- src/components/Pomodoro.js
+++ src/components/Pomodoro.js
@@ -1,4 +1,4 @@
-import React from 'react'
+import React, { useEffect, useState }from 'react'
 import '../css/Home.css';
 import {
     BrowserRouter as Router,
@@ -11,11 +11,28 @@ import {
   } from "react-router-dom";
 
 
-export function Timer (props){
-        const date = new Date().toISOString()
+export class Timer extends React.Component{
+    constructor(props){
+        super(props);
+        this.state = { time: Date };
+      }
+    componentDidMount() {
+        this.interval = setInterval(() => this.setState({ time: Date() }), 1000);
+      }
+    componentWillUnmount() {
+        clearInterval(this.interval);
+      }
+    get_current_time(){
+        let d = this.state.time
+        // console.log(d.getFullYear(), d.getMonth(), d.getDate(), d.getDay())
+    }
+      
+    render (){
         return (
             <div>
-                {date}
+                {this.state.time}
+                {/* {this.get_current_time()} */}
             </div>
             );
       };
+    }
